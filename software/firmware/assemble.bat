4f22be39894f4b94425fc148724c3d05b6717345
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2020-01-17 23:47:15 +0100

    Deorganized firmware files

--- software/firmware/assemble.bat
+++ software/firmware/assemble.bat
@@ -1,6 +1,8 @@
 @echo off
-del "system.bin"
-copy /b assembly\6530-003.bin+assembly\6530-002.bin "system.bin"
+sbasm.py 6530-003.asm
+sbasm.py 6530-002.asm
+
+copy /b 6530-003.bin+6530-002.bin "system.bin"
 
 del "28c64.bin"
 copy /b "blank_2k.bin"+"blank_2k.bin"+"blank_2k.bin"+"system.bin" "28c64.bin"
--- software/firmware/assembly/assemble.bat
+++
@@ -1,3 +0,0 @@
-sbasm.py 6530-003.asm
-sbasm.py 6530-002.asm
-pause
