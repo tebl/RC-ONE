3bcad68e76a27b59e7c9a4bd18c5716b9a0c16f1
Tor-Eirik Bakke Lunde <teblunde@gmail.com>
2020-01-18 01:03:31 +0100

    Prepare for other parts of firmware

--- software/firmware/assemble.bat
+++ software/firmware/assemble.bat
@@ -1,8 +1,8 @@
 @echo off
-sbasm.py 6530-003.asm
-sbasm.py 6530-002.asm
-
-copy /b 6530-003.bin+6530-002.bin "system.bin"
+pushd monitor
+call assemble.bat
+popd
+copy /b monitor\6530-003.bin+monitor\6530-002.bin "system.bin"
 
 del "28c64.bin"
 copy /b "blank_2k.bin"+"blank_2k.bin"+"blank_2k.bin"+"system.bin" "28c64.bin"
---
+++ software/firmware/monitor/assemble.bat
@@ -0,0 +1,3 @@
+@echo off
+sbasm.py 6530-003.asm
+sbasm.py 6530-002.asm
